345d55a4c08e3686d43d948d75a5466b01a7921a
Jordan <jordan@niau.io>
2020-07-16 10:47:29 +0800

    change eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,72 +5,69 @@ module.exports = {
     node: true,
     browser: true,
   },
-  settings: { react: { version: 'detect' } },
-  parserOptions: {
-    ecmaVersion: 2018,
-    ecmaFeatures: { jsx: true },
-    sourceType: 'module',
-  },
-  overrides: [
-    {
-      settings: {
-        'import/resolver': {
-          node: {
-            extensions: ['.ts', '.tsx'],
-            paths: ['src'],
-          },
-        },
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      'babel-module': {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
-      files: ['**/*.ts', '**/*.tsx'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 2018,
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname,
-        sourceType: 'module',
-      },
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
-      plugins: ['@typescript-eslint', 'react-hooks'],
-      rules: {
-        'react/prop-types': 'off',
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-        '@typescript-eslint/no-use-before-define': [
-          'error',
-          {
-            // hoist functions (not variables)
-            functions: false,
-          },
-        ],
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/member-delimiter-style': [
-          'error',
-          {
-            // no delimiters for interfaces
-            multiline: {
-              delimiter: 'none',
-              requireLast: false,
-            },
-            singleline: {
-              delimiter: 'comma',
-              requireLast: false,
-            },
-          },
-        ],
-        '@typescript-eslint/indent': 'off',
-        'no-unused-vars': 'off',
-        'node/no-unsupported-features/es-syntax': 'off',
-        'comma-dangle': ['error', 'always-multiline'],
-        'react/react-in-jsx-scope': 'off',
+      node: {
+        extensions: ['.ts', '.tsx'],
+        paths: ['src'],
       },
     },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    // * the eslint recommended config
+    'eslint:recommended',
+    // * plugin rules
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // 1. prettierx settings with the "standardx" style
+    'plugin:prettierx/standardx',
+    // 2. add exclusions for additional plugins
+    'plugin:prettierx/@typescript-eslint',
+    'plugin:prettierx/react',
   ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettierx'],
+  rules: {
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        // hoist functions (not variables)
+        functions: false,
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        // no delimiters for interfaces
+        multiline: {
+          delimiter: 'none',
+          requireLast: false,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false,
+        },
+      },
+    ],
+    '@typescript-eslint/indent': 'off',
+    'no-unused-vars': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'react/react-in-jsx-scope': 'off',
+  },
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,11 @@ module.exports = {
   trailingComma: 'all',
   endOfLine: 'lf',
   arrowParens: 'always',
+  singleQuote: true,
+  spaceBeforeFunctionParen: true,
+  generatorStarSpacing: true,
+  yieldStarSpacing: true,
+  semi: false,
+  jsxSingleQuote: true,
+  alignTernaryLines: false,
 }
