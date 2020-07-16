b9d779fb40ceeee3fd3f39d31942a8ba38993d99
Jordan <jordan@niau.io>
2020-07-16 11:53:55 +0800

    add attempt to remove FOUC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,22 +2,35 @@ import React from 'react'
 import Document, { Main, NextScript, Head } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+// TODO type ctx, App, and props
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }: any) {
+  static async getInitialProps (ctx: any) {
     const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-    const page = renderPage((App: any) => (props: any) =>
-      sheet.collectStyles(<App {...props} />),
-    )
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />),
+        })
 
-    const styleTags = sheet.getStyleElement()
-
-    return { ...page, styleTags }
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render () {
-    const { styleTags } = this.props as any
-
     return (
       <html lang='en'>
         <Head>
@@ -32,7 +45,6 @@ export default class MyDocument extends Document {
             content='width=device-width, initial-scale=1, shrink-to-fit=no'
           />
           <meta name='theme-color' content='#252527' />
-          {styleTags}
         </Head>
         <body>
           <Main />
@@ -42,3 +54,44 @@ export default class MyDocument extends Document {
     )
   }
 }
+
+// export default class MyDocument extends Document {
+//   static getInitialProps ({ renderPage }: any) {
+//     const sheet = new ServerStyleSheet()
+
+//     const page = renderPage((App: any) => (props: any) =>
+//       sheet.collectStyles(<App {...props} />),
+//     )
+
+//     const styleTags = sheet.getStyleElement()
+
+//     return { ...page, styleTags }
+//   }
+
+//   render () {
+//     const { styleTags } = this.props as any
+
+//     return (
+//       <html lang='en'>
+//         <Head>
+//           <link
+//             rel='shortcut icon'
+//             type='image/x-icon'
+//             href='/static/assets/favicon.ico'
+//           />
+//           <meta data-charset='utf-8' key='charset' />
+//           <meta
+//             name='viewport'
+//             content='width=device-width, initial-scale=1, shrink-to-fit=no'
+//           />
+//           <meta name='theme-color' content='#252527' />
+//           {styleTags}
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </html>
+//     )
+//   }
+// }
